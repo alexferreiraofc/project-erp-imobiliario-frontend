268613ea78ac002e0fe12941fdcd4d97e74599f2
alexferreiraofc <alex.ferreiraofc@gmail.com>
2022-12-22 11:54:38 -0300

    feat: 'added button import and sidenav'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+
+// para trabalhar com formularios angular 12
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+// para realizar requisicoes HTTP
+import {HttpClientModule} from '@angular/common/http';
+
+// imports de componentes do Angular Material
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +23,16 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    // trabalhar com formularios
+    ReactiveFormsModule,
+    // requisicoes HTTP
+    FormsModule,
+
+    // Imports angular material
+    MatButtonModule,
+    MatSidenavModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
