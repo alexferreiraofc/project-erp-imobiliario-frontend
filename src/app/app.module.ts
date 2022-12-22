4b165f601493cad0e5b55f17bbcd90ecfbacf08a
alexferreiraofc <alex.ferreiraofc@gmail.com>
2022-12-22 14:34:39 -0300

    feat: 'HTML e CSS do componente Sidenav'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,12 @@ import {HttpClientModule} from '@angular/common/http';
 // imports de componentes do Angular Material
 import {MatButtonModule} from '@angular/material/button';
 import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 // componentes do projeto
 import { NavComponent } from './components/nav/nav.component';
@@ -36,7 +42,14 @@ import { NavComponent } from './components/nav/nav.component';
     // Imports angular material
     MatButtonModule,
     MatSidenavModule,
-
+    MatIconModule,
+    MatListModule,
+    MatToolbarModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule,
+    
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
