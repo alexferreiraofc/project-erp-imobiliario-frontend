8975bcd3797dd1a0bdb786034e303f1e93e3564a
alexferreiraofc <alex.ferreiraofc@gmail.com>
2022-12-22 17:45:02 -0300

    adicionado as rotas para: vendas, pessoas, imoveis

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { ImoveisListComponent } from './components/imoveis/imoveis-list/imoveis-list.component';
 import { NavComponent } from './components/nav/nav.component';
+import { PessoasListComponent } from './components/pessoas/pessoas-list/pessoas-list.component';
+import { VendasListComponent } from './components/vendas/vendas-list/vendas-list.component';
 
 const routes: Routes = [
   {
     path: '', component: NavComponent, children: [
-      {path: 'home', component: HomeComponent}
+      {path: 'home', component: HomeComponent},
+      {path: 'vendas', component: VendasListComponent},
+      {path: 'pessoas', component: PessoasListComponent},
+      {path: 'imoveis', component: ImoveisListComponent},
     ]
   },
 
